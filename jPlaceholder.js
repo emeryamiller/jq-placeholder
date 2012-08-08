ffbe1c5362d9c97948db46ea5e95e5b824b0a5ae
Emery Miller <emery@opusworkspace.com>
2012-08-08 13:33:34 -0400

    Refactoring code and adding a 1 pixel padding by default

--- jPlaceholder.js
+++ jPlaceholder.js
@@ -1,12 +1,29 @@
 ;(function($) {
-  var pixels_to_int = function(pixels) {
-    return parseInt(pixels.replace('px', ''));
-  };
 
 	$.fn.placeholder = function(options) {
-		var opts = $.extend({}, $.fn.placeholder.defaults, options);
+    var pixels_to_int = function(pixels) {
+      return parseInt(pixels.replace('px', ''));
+    };
+    var field_padding = function($fld) {
+      result = {}
+      $.each(['top', 'left', 'right', 'bottom'], function(index, area) {
+        result[area] = 0;
+        $.each(['margin', 'border', 'padding'], function(index, kind) {
+          result[area] += pixels_to_int($fld.css(kind+'-'+area));
+        });
+      });
+      return result;
+    };
+
 		return this.each(function() {
+      var opts = $.extend({}, $.fn.placeholder.defaults, options);
 			var $field = $(this);
+      if ($field.data('placeholder_active')) {
+        keystorke_handler();
+        return;
+      } else {
+        $field.data('placeholder_active', true);
+      }
       var $label;
       var placeholder_text = $field.data('placeholder');
       if (!placeholder_text) {
@@ -17,25 +34,27 @@
       if (!placeholder_text) { return; }
       $label = $('<span class="placeholder">' + placeholder_text + '</span>');
       $field.after($label);
-      if (!opts.slide) { opts.vanishing_length = 0; }
       $field.parent().css({ position: 'relative' })
       $label.css({position: 'absolute'});  // Remove it from the flow
 
 			var field_pos = $field.position();
-			var label_pos = $field.position();
-      var field_margin_top = pixels_to_int($field.css('margin-top'));
-      var field_margin_left = pixels_to_int($field.css('margin-left'));
-      var field_margin_right = pixels_to_int($field.css('margin-right'));
-      var field_padding_top = pixels_to_int($field.css('padding-top'));
-      var field_padding_left = pixels_to_int($field.css('padding-left'));
-      var field_padding_right = pixels_to_int($field.css('padding-right'));
+      var field_surround = field_padding($field);
+      var label_top = field_pos.top + field_surround.top + opts.padding_top;
+      var label_left = field_pos.left + field_surround.left + opts.padding_left;
+      var label_right = field_pos.left + $field.outerWidth() - field_surround.right - opts.padding_right;
+      var label_rightmost_left = label_right - $label.outerWidth();
 
-			var starting_position = label_pos.left + field_padding_left + opts.padding_left + field_margin_left;
-			var ending_position = starting_position + $field.width() - field_padding_right - opts.padding_right - field_margin_right;
+      if (label_rightmost_left < label_left) {
+        label_rightmost_left = label_left;
+        opts.slide = false;
+      }
+
+      if (!opts.slide) { opts.vanishing_length = 0; }
+
       var current_length = 0;
-      var read_value = $field.val;
       var tagname = $field.get(0).tagName.toLowerCase();
-      if (tagname == 'div') { read_value = $field.text; }
+      var read_value = $field.val;
+      if (!(tagname === 'input' || tagname === 'textarea')) { read_value = $field.text; }
 
       var keystroke_handler = function(e) {
           current_length = read_value.call($field).length
@@ -48,8 +67,8 @@
       }
 
 			$label.css({
-				top: label_pos.top + field_padding_top + opts.padding_top + field_margin_top,
-				left: starting_position
+				top: label_top,
+				left: label_left
 			}).mousedown(function(e) {
         e.preventDefault();
         $field.focus();
@@ -58,13 +77,13 @@
 			$field.addClass('placeheld').focus(function() {
         if (opts.slide) {
           $label.stop().animate({
-            left: ending_position,
+            left: label_rightmost_left
           }, opts.focus_speed);
 		  	}
       }).blur(function() {
         if (!current_length && opts.slide) {
           $label.stop().animate({
-            left: starting_position,
+            left: label_left
           }, opts.blur_speed);
         }
       }).bind('keyup keypress', keystroke_handler);
@@ -81,7 +100,7 @@
 		focus_speed: 600,
 		blur_speed: 600,
 		padding_top: 0,
-		padding_left: 0,
+		padding_left: 1,
 		padding_right: 0,
 		padding_bottom: 0,
     vanishing_length: 5,
